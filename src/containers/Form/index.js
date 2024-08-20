73cecf4c95eea34d7331fa7d78c90105788de7ac
steve <stevetak@outlook.fr>
2024-08-20 02:21:20 +0200

    appel de la fonction onSuccess

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState} from "react";
 import PropTypes from "prop-types";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
@@ -8,14 +8,17 @@ const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500)
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
+ 
+
   const sendContact = useCallback(
     async (evt) => {
       evt.preventDefault();
       setSending(true);
-      // We try to call mockContactApi
+      
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess();
       } catch (err) {
         setSending(false);
         onError(err);
@@ -49,6 +52,7 @@ const Form = ({ onSuccess, onError }) => {
           />
         </div>
       </div>
+
     </form>
   );
 };
